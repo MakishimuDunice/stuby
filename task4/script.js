0ec75b43b8e508bc58b642fd25210d77ed18cf47
Makishimu <kudryavtsev.maxim.dunice@gmail.com>
2014-08-28 13:14:39 +0400

    Add task 4

---
+++ task4/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function (){
+    $('#input-field').keyup( function() {
+        var $this = $(this);
+        if($this.val().length > 50){
+            $('#dick').removeClass("none");
+        }
+        else {
+            $('#dick').addClass('none');
+        }
+    });
+
+    $('#button').click(function() {
+        if ($('#input-field').val().length > 50) {
+            $('#dick').removeClass("none");
+        }
+        else if ($('#input-field').val().length) {
+            var toAdd = $('input[name=checkListItem]').val();
+            $('.list').append('<li id="item">' + toAdd + '</li');
+            $('#input-field').val('');
+            $('#dick').addClass('none');
+
+        }
+    });
+});
